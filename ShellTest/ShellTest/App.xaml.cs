cba8609742e813ee98d8764f0b22ea592c18b5e9
Scott Kuhl <scott@kuhl.ws>
2020-07-21 22:14:11 -0500

    Add project files.

---
+++ ShellTest/ShellTest/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Xamarin.Forms;
+
+namespace ShellTest
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+
+        protected override void OnResume()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnStart()
+        {
+        }
+    }
+}
---
+++ ShellTest/ShellTest/AppShell.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ShellTest
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
